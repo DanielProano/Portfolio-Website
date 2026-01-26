48b2a8529232577fe388d5a59ccc93197ec53bf7
Danny <179722984+DanielProano@users.noreply.github.com>
2026-01-26 12:15:27 -0500

    Updated Portfolio so as to not have any tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 
 import config from "./src/config.json";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     host: "0.0.0.0",
     port: parseInt(config.port) || 5173,
